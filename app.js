3b6f93993403880b3986273b4335800c66f52508
Kpennie <kpennie0@gmail.com>
2025-07-16 21:38:40 +0000

    Exercise unit test with jest

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// let oneEuroIs = {
+//     "JPY": 156.5, // japan yen
+//     "USD": 1.07, // us dollar
+//     "GBP": 0.87, // british pound
+// }
+
+
 const fromEuroToDollar = function(valueInEuro) {
-    let valueInDollar = valueInEuro * 1.07; // Convert euros to dollars
-    return valueInDollar; // Return the converted value
+    let valueInDollar = valueInEuro * 1.07; 
+    return valueInDollar; 
 }
 
 const fromDollarToYen = function(valueInDollar) {
@@ -8,7 +15,7 @@ const fromDollarToYen = function(valueInDollar) {
     return valueInYen; // Return the converted value
     
 }
- //test2
+ 
 const fromYenToPound = function(valueInYen) {
     let valueInDollar = valueInYen / 156.5; // Convert yen to dollars
     let valueInPound = valueInDollar * 0.87; // Convert dollars to pounds
